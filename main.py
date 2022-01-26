2edfca324922d9cc2da34cb00483aba6acaf68bf
Aflynn50 <alastairflynn@hotmail.co.uk>
2022-01-26 22:47:45 +0000

    Working version

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import string
 import pickle
-from collections import OrderedDict
+import hashlib
 from collections import defaultdict
 import nltk
 nltk.download('stopwords')
@@ -13,41 +13,18 @@ import threading
 import time
 stemmer = SnowballStemmer("english")
 stop_words = set(stopwords.words('english'))
+
 recipes_dir = './recipes'
-recipes_list = os.listdir(recipes_dir)
-
-
 saved_dict_location = './index/reversed_index.pkl'
+
 title_w = 100
 intro_w = 5
 ingrident_w = 20
 method_w = 10
 
-
-
-# return all title matches first, then any matches in the ingredients, then description
-# plan:
-#   Keep an index of words in a dictionary we load from storage
-#   Build a revse index, each word links to documents its in
-#   if the recipe list changes we can update the index in the background
-
 # The keys of the rev_index are words. The values are a list of pairs, first one is recipe id second is score for that word 
 
 # Weights: Title: 100, ingridents 20, description 10, introduction 10
-
-# Hashing a directory name list
-# hash(" ".join(os.listdir('./recipes')))
-
-# stopwords:
-# print(stopwords.words('english'))
-
-# saving a dictionary
-# with open('saved_dictionary.pkl', 'wb') as f:
-#     pickle.dump(dictionary, f)
-        
-# with open('saved_dictionary.pkl', 'rb') as f:
-#     loaded_dict = pickle.load(f)
-
 
 # convert to lower case, remove punctuation, split, filter out stop words, stem the list of words
 # then remove duplicates
@@ -65,34 +42,31 @@ def add_line_to_dict(rid,line,dic,weight):
 
 def assert_sec_title(line,t,recipe):
     if line != t:
-        print("Recipe " + recipe + " is incomplete")
+        pass#print("Recipe " + recipe + " is incomplete")
 
 def const_empty_list():
     return []
 
 def build_reverse_index():
     word_index = defaultdict(const_empty_list,[])
-    #id_to_recipie = dict(zip(range(0,len(recipes_list)),recipes_list))
     id_to_recipie = {} # Map the id to the title of the recipe
     recipe_id = 0
-    for recipe in recipes_list: 
+    for recipe in os.listdir(recipes_dir): 
         with open('./recipes/' + recipe, 'r') as lines:
+            # Go through the recipe adding each line to the dictionary with the appropriate weight
             line = lines.readline() # The title
-            id_to_recipie[recipe_id] = line
+            id_to_recipie[recipe_id] = line # Assosiate the recipie id with its title
             add_line_to_dict(recipe_id,line,word_index,title_w)
 
             assert_sec_title(lines.readline(),"Introduction:\n",recipe)
-
             line = lines.readline()
             add_line_to_dict(recipe_id,line,word_index,intro_w)
 
             assert_sec_title(lines.readline(),"Ingredients:\n",recipe)
-
             line = lines.readline()
             add_line_to_dict(recipe_id,line,word_index,ingrident_w)
 
             assert_sec_title(lines.readline(),"Method:\n",recipe)
-
             line = lines.readline()
             add_line_to_dict(recipe_id,line,word_index,method_w)
 
@@ -100,6 +74,7 @@ def build_reverse_index():
     
     return (word_index,id_to_recipie)
 
+# Algorithm to merge the lists of (recipe_id,score) pairs from the different words of the query
 def merge_ordered_lists_of_pairs(xss): # merge the scores
     if xss == []:
         return []
@@ -134,7 +109,7 @@ def perform_search(query: str, rev_index = None, ids=None) -> [str]:
     top_recipes = number_items(list(map(lambda x: ids[x[0]] + "  Score: " + str(x[1]), top_recipe_ids)))
     return top_recipes
 
-def search_loop(reload_index,index_lock,check_index):
+def search_loop(reload_index,check_index,index_lock):
     reload_index.wait() # wait for inital reload
     while True:
         if reload_index.is_set(): # check if we need to reload
@@ -143,6 +118,7 @@ def search_loop(reload_index,index_lock,check_index):
                 (rev_index,ids,_) = pickle.load(f)
             index_lock.release()
             reload_index.clear() # set reload index to clear as we have reloaded
+            print("New index loaded!")
         print("Please enter search query: ")
         query = input()
         tic = time.perf_counter()
@@ -150,26 +126,37 @@ def search_loop(reload_index,index_lock,check_index):
         toc = time.perf_counter()
         print("The results are in!:\n")
         print("\n".join(top_recipes))
-        print(f"searched in {toc - tic:0.4f} seconds\n\n")
+        print(f"\nsearched in {toc - tic:0.4f} seconds\n\n")
         check_index.set() # tell the other thread to check the index, just in case its changed recently
+
+def hash_files(file_list):
+    return hashlib.sha1((" ".join(file_list)).encode()).hexdigest()
 
 def index_loop(reload_index,check_index,index_lock):
     index_lock.acquire()
-    with open(saved_dict_location, 'rb') as f:
-        (_,_,recipes_hash) = pickle.load(f) # get the inital recipe hash
+    recipes_hash = 0
+    if os.path.isfile(saved_dict_location):
+        with open(saved_dict_location, 'rb') as f:
+            (_,_,recipes_hash) = pickle.load(f) # get the inital recipe hash
+        if hash_files(os.listdir(recipes_dir)) == recipes_hash:
+            reload_index.set()
     index_lock.release()
+    
 
     while True:
         check_index.wait() # only check after a search so as not to waste looping here
-        current_recipes_hash = hash(" ".join(os.listdir(recipes_dir)))
-        if current_recipes_hash != recipes_hash:
-            index_lock.aquire()
+        current_recipes_hash = hash_files(os.listdir(recipes_dir))
 
+        if current_recipes_hash != recipes_hash:
+            index_lock.acquire()
+            
+            #print("\nRefreshing index\n")
             (word_index,id_to_recipie) = build_reverse_index() # build new index
+            #print("\nIndex refrehsed!1!!\n")
             with open(saved_dict_location, 'wb') as f:
                 pickle.dump((word_index,id_to_recipie,current_recipes_hash), f) # save index to file
             recipes_hash = current_recipes_hash
-            
+
             index_lock.release()
             reload_index.set() # tell search loop that it needs to reload
         check_index.clear() # reset the check index since we have just checked
@@ -180,9 +167,10 @@ def main():
     reload_index = threading.Event()
     check_index = threading.Event()
     index_lock = threading.Lock()
+
     reload_index.clear()
     check_index.set()
-    #index_lock.release()
+
     search = threading.Thread(target=search_loop,args=(reload_index,check_index,index_lock),name='search')
     index = threading.Thread(target=index_loop,args=(reload_index,check_index,index_lock),name='index')
     index.start()
